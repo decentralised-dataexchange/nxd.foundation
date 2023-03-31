c0644b4099493e7cb4a713161dee60901296a728
George J Padayatti <george.padayatti@igrant.io>
2023-04-01 02:13:01 +0530

    NXD landingpage with updated header, hero, footer
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Footer from '../components/Footer';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+    <header className={clsx('hero hero--primary flex-auto p-0')}>
+      <div className="container absolute top-[150px] md:top-[225px]">
+        <div className={styles.buttons + ['flex']}>
+          <p className='text-3xl md:text-5xl w-[300px] md:w-[1000px] md:text-justify break-words whitespace-normal'>NXD: Enabling trust in the data ecosystem for next-generation data sharing.</p>
+          <p className='mt-10 text-xl md:text-2xl w-[300px] md:w-[800px] md:text-justify bg-white text-black break-words'>Trust within a dataspace is a requisite for safe and secure data sharing.</p>
         </div>
       </div>
     </header>
@@ -30,12 +24,13 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Enabling trust in the data ecosystem for next-generation data sharing. ">
       <HomepageHeader />
-      <main>
+      {/* <main>
         <HomepageFeatures />
-      </main>
+      </main> */}
+      <Footer />
     </Layout>
   );
 }
