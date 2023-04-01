198fd9f5564ccb120e0cc0b4496a0ea39e4223c0
George J Padayatti <george.padayatti@igrant.io>
2023-04-01 19:39:56 +0530

    Fix hero tag-line background color
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary flex-auto p-0')}>
       <div className="container absolute top-[150px] md:top-[225px]">
         <div className={styles.buttons + ['flex']}>
-          <p className='text-3xl md:text-5xl w-[300px] md:w-[1000px] md:text-justify break-words whitespace-normal'>NXD: Enabling trust in the data ecosystem for next-generation data sharing.</p>
-          <p className='mt-10 text-xl md:text-2xl w-[300px] md:w-[800px] md:text-justify bg-white text-black break-words'>Trust within a dataspace is a requisite for safe and secure data sharing.</p>
+          <p className='text-3xl md:text-5xl w-full md:w-4/6 text-left lg:text-justify break-words whitespace-normal'>NXD: Enabling trust in the data ecosystem for next-generation data sharing.</p>
+          <p className='mt-10 text-xl md:text-2xl w-full md:w-4/6 text-left  lg:text-justify text-black break-words'><span className='inline bg-white'>Trust within a dataspace is a requisite for safe and secure data sharing.</span></p>
         </div>
       </div>
     </header>
