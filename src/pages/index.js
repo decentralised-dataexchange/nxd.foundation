323a8c57a7a00587c0b2571d646c22ee95789161
George J Padayatti <george.padayatti@igrant.io>
2023-04-03 21:27:49 +0530

    Update opacity for hero text tagline
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
       <div className="container absolute top-[150px] md:top-[225px]">
         <div className={styles.buttons + ['flex']}>
           <p className='text-3xl md:text-5xl w-full md:w-4/6 text-left break-words whitespace-normal'><Translate>NXD: Enabling trust in the data ecosystem for next-generation data sharing.</Translate></p>
-          <p className='mt-10 text-xl md:text-2xl w-full md:w-4/6 text-left opacity-60 text-black break-words'><span className='inline bg-white'><Translate>Trust within a dataspace is a requisite for safe and secure data sharing.</Translate></span></p>
+          <p className='mt-10 text-xl md:text-2xl w-full md:w-4/6 text-left opacity-70 text-black break-words'><span className='inline bg-white'><Translate>Trust within a dataspace is a requisite for safe and secure data sharing.</Translate></span></p>
         </div>
       </div>
     </header>
