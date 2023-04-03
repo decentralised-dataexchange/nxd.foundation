d3d4102f96ffaf7d2359a57a66b2142145fee4fd
George J Padayatti <george.padayatti@igrant.io>
2023-04-03 20:28:04 +0530

    Update indy testnet button label
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/theme/NavbarItem/ComponentTypes.js
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -21,7 +21,7 @@ function IndyNavigationBtn(props) {
       className="inline-block rounded-full border border-white bg-white text-[12px] px-3 py-2 md:px-3 md:py-2 md:text-[12px] font-medium text-black hover:bg-transparent hover:no-underline hover:text-white focus:outline-none focus:ring active:text-black"
       href="https://indy.nxd.foundation/"
     >
-      Indy testnet
+      Indy Ledger
     </a>
   );
 }
