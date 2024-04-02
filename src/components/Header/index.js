4c6fa8a6c95db1385149379b7eee3797d7569d65
George J Padayatti <george.padayatti@igrant.io>
2024-04-02 18:31:02 +0530

    Fix: Revert to old hero texts
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Translate from '@docusaurus/Translate';
 import clsx from 'clsx';
+import styles from './../../pages/index.module.css';
 
 export default function Header() {
     return (
-        <header className={clsx('hero hero--primary flex-auto p-0 min-h-[80dvh')}>
-            <div className="container flex flex-col justify-center items-center">
-                <p className='mt-5 md:mt-0 text-3xl md:text-5xl w-full md:w-4/6 text-center break-words whitespace-normal'><Translate>CRANE Decentralised Health Dataspace Infrastructure</Translate></p>
-                <p className='mt-10 text-lg md:text-2xl w-full md:w-4/6 text-center opacity-70 text-black break-words'><span className='inline bg-white'><Translate>Developer Documentation and APIs</Translate></span></p>
-                <p className='mb-5 md:mb-0 text:lg md:text-2xl w-full md:w-4/6 text-center opacity-70 text-black break-words'><span className='inline bg-white'><Translate>Onboard, Integrate with and extend via the decentralised health data space</Translate></span></p>
+        <header className={clsx('hero hero--primary flex-auto p-0 min-h-screen')}>
+            <div className="container absolute top-[150px] md:top-[225px]">
+                <div className={styles.buttons + ['flex']}>
+                    <p className='text-3xl md:text-5xl w-full md:w-4/6 text-left break-words whitespace-normal'><Translate>NXD: Enabling trust in the data ecosystem for next-generation data sharing.</Translate></p>
+                    <p className='mt-10 text-xl md:text-2xl w-full md:w-4/6 text-left opacity-70 text-black break-words'><span className='inline bg-white'><Translate>Trust within a dataspace is a requisite for safe and secure data sharing.</Translate></span></p>
+                </div>
             </div>
         </header>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -41,18 +41,6 @@ export default function Home() {
       <Helmet>
         <title>NordXDataspace (NXD)</title>
       </Helmet>
-      <div className='container mx-auto flex flex-col mt-4 mb-4'>
-        <div className='flex flex-col sm:flex-row mt-4 mb-4 space-y-4 sm:space-y-0 sm:space-x-4'>
-          {CardList.map((props, idx) => (
-            <div className="w-full sm:w-1/3 flex">
-              <Card key={idx} {...props} />
-            </div>
-          ))}
-        </div>
-        <div className="p-0 w-full lg:flex">
-          <Card title={"Trust Anchor"} link={"https://indy.nxd.foundation"} />
-        </div>
-      </div>
       <Footer />
     </Layout>
   );
